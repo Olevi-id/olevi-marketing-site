2823b1e232087ba76fbd424170b33975ba3f2434
Kari Laalo <kari@karidea.eu>
2026-02-09 21:33:09 +0200

    Refactor: Simplify renderScripts logic and remove unused Bootstrap scrollspy initialization in scripts.js

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -8,17 +8,16 @@ const __dirname = path.dirname(__filename);
 
 export default function renderScripts() {
     const sourcePath = path.resolve(__dirname, '../src/js');
-    const destPath = path.resolve(__dirname, '../dist');
-
-    fs.cpSync(sourcePath, destPath, { recursive: true });
-
-    const bootstrapJS = path.resolve(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
-    const bootstrapJSMap = path.resolve(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map');
     const destJSPath = path.resolve(__dirname, '../dist/js');
 
     if (!fs.existsSync(destJSPath)) {
         fs.mkdirSync(destJSPath, { recursive: true });
     }
+
+    fs.cpSync(sourcePath, destJSPath, { recursive: true });
+
+    const bootstrapJS = path.resolve(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
+    const bootstrapJSMap = path.resolve(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map');
 
     fs.copyFileSync(bootstrapJS, path.join(destJSPath, 'bootstrap.bundle.min.js'));
     fs.copyFileSync(bootstrapJSMap, path.join(destJSPath, 'bootstrap.bundle.min.js.map'));
